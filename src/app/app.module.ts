b2b0fcdb5d96d00ed638b78deeffa39ed73532d3
Inza124 <stachaczynski.piotr.prv@gmail.com>
2018-10-02 13:01:15 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { UploadComponent } from './upload/upload.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImageDetailComponent,
+    NavbarComponent,
+    LoginComponent,
+    UploadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
